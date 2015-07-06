cf49f86b6d2f21349676465ff5c9eac55c38da2f
Arjun Munji <arjunmunji@hotmail.com>
2015-07-07 02:16:07 +0530

    stuff

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.set('view engine', 'ejs');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(session({
-  secret: 'keyboard cat'
+  secret: 'keyboardcat'
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
